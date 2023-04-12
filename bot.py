6d0ed290e985cf33e16747dfb31fc5d4ae105cd7
codertang <tangchristopher111@gmail.com>
2023-04-11 20:22:42 -0700

    reminder feature

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 #bot.py
-import os, discord, dotenv, random
+import os, discord, dotenv, random, datetime
 
 dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -7,6 +7,8 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 what_words = ['what', 'wat', 'waht', 'wjat']
 anime_ref = ['WEEB ALERT!', 'A WILD WEEB HAS APPEARED', 'IS THIS A JOJO REFERENCE?']
+
+timezone = datetime.timezone(datetime.timedelta(hours=-8))
 
 client = discord.Client(intents = discord.Intents.all())
 
@@ -33,9 +35,17 @@ async def on_message(message):
             await message.reply('CHICKEN POO :rofl:')
     if 'stfu' in message.content.lower():
             await message.reply('no u')
+
     with open('weeb.txt', 'r') as fin:
         for word in fin.read().split('|'):
             if word in message.content.lower():
                 await message.reply(random.choice(anime_ref))
-
+    for user in message.mentions:
+        if user.id == 752956559885205697:
+            current_time = datetime.datetime.now(timezone)
+            target_time = current_time.replace(hour=22, minute=0, second=0, microsecond=0)
+            if current_time < target_time:
+                nickname = message.author.nick if message.author.nick else message.author.name
+                await message.reply(f"{nickname} is currently focusing on homework, please do not ping until after 10:00 PM PST.")
+            
 client.run(TOKEN)
